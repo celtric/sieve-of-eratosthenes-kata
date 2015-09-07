dae93c4951aaedd9937cea1939c828f67c25339a
Ricard Osorio <celtric@gmail.com>
2015-09-07 10:07:54 +0200

    discard multiples of previously identified prime

--- src/main/java/org/celtric/katas/sieveoferatosthenes/PrimeFinder.java
+++ src/main/java/org/celtric/katas/sieveoferatosthenes/PrimeFinder.java
@@ -2,7 +2,32 @@ package org.celtric.katas.sieveoferatosthenes;
 
 public class PrimeFinder {
 
+    static final int UNCHECKED = 0;
+    static final int PRIME = 1;
+    static final int NOT_PRIME = 2;
+
+    int[] primeCandidates;
+
     public int[] findPrimes(int limit) {
+        primeCandidates = new int[limit + 1];
+
+        for (int i = 2; i <= limit; i++) {
+            if (primeCandidates[i] != UNCHECKED) {
+                continue;
+            }
+
+            primeCandidates[i] = PRIME;
+            disableMultiplesOf(i);
+        }
+
         return new int[]{};
+    }
+
+    private void disableMultiplesOf(int i) {
+        for (int n = i + 1; n < primeCandidates.length; n++) {
+            if (n % i == 0) {
+                primeCandidates[n] = NOT_PRIME;
+            }
+        }
     }
 }
--- src/test/java/org/celtric/katas/sieveoferatosthenes/PrimeFinderTest.java
+++ src/test/java/org/celtric/katas/sieveoferatosthenes/PrimeFinderTest.java
@@ -18,4 +18,15 @@ public class PrimeFinderTest {
     public void it_returns_an_array_of_integers() {
         assertArrayEquals(new int[]{}, finder.findPrimes(0));
     }
+
+    @Test
+    public void it_discards_composites_of_2() {
+        finder.findPrimes(10);
+
+        assertEquals(PrimeFinder.PRIME, finder.primeCandidates[2]);
+        assertEquals(PrimeFinder.NOT_PRIME, finder.primeCandidates[4]);
+        assertEquals(PrimeFinder.NOT_PRIME, finder.primeCandidates[6]);
+        assertEquals(PrimeFinder.NOT_PRIME, finder.primeCandidates[8]);
+        assertEquals(PrimeFinder.NOT_PRIME, finder.primeCandidates[10]);
+    }
 }
