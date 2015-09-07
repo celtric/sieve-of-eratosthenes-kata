d6d1da3e6d0f89cb4b5158c3b4346f43b42c237d
Ricard Osorio <celtric@gmail.com>
2015-09-07 10:42:51 +0200

    increased test confidence

--- src/test/java/org/celtric/katas/sieveoferatosthenes/PrimeFinderTest.java
+++ src/test/java/org/celtric/katas/sieveoferatosthenes/PrimeFinderTest.java
@@ -49,4 +49,15 @@ public class PrimeFinderTest {
     public void it_finds_prime_numbers_up_to_5() {
         assertArrayEquals(new int[]{2, 3, 5}, finder.findPrimes(5));
     }
+
+    @Test
+    public void it_finds_prime_numbers_up_to_10() {
+        assertArrayEquals(new int[]{2, 3, 5, 7}, finder.findPrimes(10));
+    }
+
+    @Test
+    public void it_finds_prime_numbers_up_to_100() {
+        int[] expected = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+        assertArrayEquals(expected, finder.findPrimes(100));
+    }
 }
